50fbe354bbca791d4fbcc68e2a3fd3f5e47ecffe
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-03-01 04:18:36 +0600

    feat: find minimum, maximum and average of array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-
 exports.min = function min (array) {
+  if (array && array.length) {    
+    array.sort((a,b) => a-b);
+  const min = array[0];
+  return min;
+ } else { 
   return 0;
+ } 
 }
 
 exports.max = function max (array) {
+  if (array && array.length) {    
+  array.sort((a,b) => a-b);
+  const max = array[array.length-1];
+  return max;
+  } else { 
   return 0;
+  } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array && array.length) {    
+    return array.reduce((a, b) => (a + b)) / array.length;
+  } else { 
+    return 0;
+  } 
+
 }
